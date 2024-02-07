4e8ca179d8583cb247e9413e59d0280d4e98f10e
= <=>
2024-02-07 08:33:56 +0300

    fix: login function

--- src/pages/components/login/index.jsx
+++ src/pages/components/login/index.jsx
@@ -1,16 +1,39 @@
+import { FormEvent } from "react"
 import { useRouter } from "next/router"
 
 export default function Login(){
-    const signUp = useRouter()
+    const router = useRouter()
+
+    async function handleSubmit(evnet){
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget)
+        const email = formData.get('email')
+        const password = formData.get('password')
+
+        const response = await fetch('/api/auth/login', {
+            method: 'POST',
+            headers: {'content type' : 'application/json'},
+            body: JSON.stringify({email, password}),
+        })
+
+        if(response.ok){
+            router.push('/progile')
+        }else{
+
+        }
+    }
     return(
         <div>
+            <form onSubmit={handleSubmit}>
             se connecter
-            <div>
-                <input type="text" placeholder="Enter your email adress" name="email" required/>
-                <input type="password" placeholder="Enter Password" name="psw" required />
-                <button type="submit">Login</button>
-                <button type="submit" onClick={()=> signUp.push("/components/signUp")} >Sign Up</button>
-            </div>
+                <div>
+                    <input type="text" placeholder="Enter your email adress" name="email" required/>
+                    <input type="password" placeholder="Enter Password" name="psw" required />
+                    <button type="submit">Login</button>
+                    <button type="submit" onClick={()=> router.push("/components/signUp")} >Sign Up</button>
+                </div>
+            </form> 
         </div>
     )
 }

0f0efe4d63e250a389e2a2361e8143b53bcc0176
= <=>
2024-02-07 08:35:00 +0300

    fix: login evnet to event

--- src/pages/components/login/index.jsx
+++ src/pages/components/login/index.jsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router"
 export default function Login(){
     const router = useRouter()
 
-    async function handleSubmit(evnet){
+    async function handleSubmit(event){
         event.preventDefault()
 
         const formData = new FormData(event.currentTarget)

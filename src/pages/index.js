ceab2c10b8448da4f2e4eec9cac9e28ead755626
Rafaliarison <124917384+Rafaliarison@users.noreply.github.com>
2024-02-18 23:25:04 +0300

    fix localhost:8080

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function SignUpLogIn() {
       const password = formData.get('password')
 
       try {
-          const response = await axios.post('http://localhost8080/login', {
+          const response = await axios.post('http://localhost:8080/login', {
             email: email,
             password: password
           });
